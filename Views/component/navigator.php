665df3d838097c8369959c5975d0cf08b73b0138
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-03-22 17:46:08 +0900

    add:notification indicator.

--- Views/component/navigator.php
+++ Views/component/navigator.php
@@ -1,11 +1,16 @@
 <div class="bg-white border-b border-gray-200 fixed top-0 left-0 right-0 flex justify-between items-center p-4 w-full max-w-screen-lg mx-auto">
   <h1 class="text-2xl font-bold text-blue-400">SNS</h1>
   <nav>
-    <ul class="flex space-x-4">
+    <ul class="flex space-x-4 items-center">
       <?php if ($user) : ?>
         <li><a href="/home" class="text-gray-600 hover:text-blue-400">ホーム</a></li>
         <li><a href="/edit/profile" class="text-gray-600 hover:text-blue-400">プロフィール</a></li>
-        <li><a href="/notification" class="text-gray-600 hover:text-blue-400">通知</a></li>
+        <li class="relative">
+          <a href="/notification" class="text-gray-600 hover:text-blue-400">
+            通知
+          </a>
+          <div class="h-4 w-4 bg-blue-400 rounded-full absolute -top-1 left-5" />
+        </li>
         <li><a href="/message" class="text-gray-600 hover:text-blue-400">メッセージ</a></li>
         <?php if ($user) : ?>
           <li><a href="/logout" class="text-gray-600 hover:text-blue-400">ログアウト</a></li>
