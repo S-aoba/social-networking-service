1db8f482ae3f729597cc0663a03a4d4897232920
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-03-22 13:14:55 +0900

    change: method name get to getById.

--- Database/DataAccess/Interfaces/NotificationDAO.php
+++ Database/DataAccess/Interfaces/NotificationDAO.php
@@ -9,6 +9,6 @@ interface NotificationDAO
   public function insert(Notification $notification): bool;
   public function update(Notification $notification): bool;
   public function delete(Notification $notification): bool;
-  public function get(int $id): ?Notification;
+  public function getById(int $id): ?Notification;
   public function getAll(): array;
 }
