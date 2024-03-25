38bacb9e493819e7d22e2061806059a8d4642857
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-03-25 22:16:56 +0900

    add: post-liked color.

--- Views/page/home.php
+++ Views/page/home.php
@@ -78,8 +78,9 @@
               <input type="hidden" name="post_id" value="<?= $data['post']->getId() ?>">
               <input type="hidden" name="post_user_id" value="<?= $data['post']->getUserId() ?>">
               <button type="submit">
-                <div class="h-8 w-8 flex items-center justify-center cursor-pointer hover:bg-slate-200 transition-colors duration-300 rounded-full">
+                <div class="h-8 w-8 flex items-center justify-center space-x-1 cursor-pointer hover:bg-slate-200 transition-colors duration-300 rounded-full">
                   <img class="h-4 w-4" src="/images/post-liked-icon.svg" alt="いいね">
+                  <span class="text-[#f0609f]"><?= $data['postLikeCount'][0]["COUNT(*)"] ?></span>
                 </div>
               </button>
             </form>
