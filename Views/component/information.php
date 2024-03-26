02eb6e8b6d315dc15312ac9244faa43ec6149729
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-03-26 22:41:32 +0900

    rm:hidden

--- Views/component/information.php
+++ Views/component/information.php
@@ -1,3 +1,3 @@
-<div class="lg:col-span-1 hidden lg:block hidden h-full pr-4 md:pr-6 bg-orange-400">
+<div class="lg:col-span-1 hidden lg:block h-full pr-4 md:pr-6 bg-orange-400">
   Information
 </div>
