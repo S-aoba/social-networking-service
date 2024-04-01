0dc91d480d300669eba6b77db57be4a4103f13cf
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-04-01 14:33:27 +0900

    fix:logic

--- Helpers/FileHelper.php
+++ Helpers/FileHelper.php
@@ -56,20 +56,13 @@ class FileHelper
 
   public static function saveFilePathInUploadsDir(string $hashed_file_path, string $file_type): void
   {
-    // 保存するFileのpathを取得
-    $target_file = self::getUploadFilePath($hashed_file_path, $file_type);
-
-    // $parent_dirが存在していなければ作成
-    // 違うファイルであっても、ハッシュ化されたファイル名の頭文字2個が同じ場合がある
-    if (!is_dir($target_file)) {
-      mkdir($target_file, 0777, true);
-    }
+    $uploads_file_path = self::getUploadFilePath($hashed_file_path, $file_type);
 
     // 保存
-    move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
+    move_uploaded_file($_FILES["image"]["tmp_name"], $uploads_file_path);
   }
 
-  public static function getUploadFilePath(string $hashed_file_path, string $file_type): string
+  public static function getUploadDir(string $hashed_file_path, string $file_type)
   {
     // root_dirを取得
     $root_dir = match ($file_type) {
@@ -80,8 +73,22 @@ class FileHelper
     // $hashed_file_pathの頭文字2個を使って親ディレクトリ名を取得
     $parent_dir = substr($hashed_file_path, 0, 2);
 
+    $dir_path = $root_dir . $parent_dir;
+    // $parent_dirが存在していなければ作成
+    // 違うファイルであっても、ハッシュ化されたファイル名の頭文字2個が同じ場合がある
+    if (!is_dir($dir_path)) {
+      mkdir($dir_path, 0777, true);
+    }
+
+    return $dir_path;
+  }
+
+  public static function getUploadFilePath(string $hashed_file_path, string $file_type): string
+  {
+
+    $dir = self::getUploadDir($hashed_file_path, $file_type);
     // 保存するFileのpathを取得
-    return $root_dir . $parent_dir . '/' . $hashed_file_path;
+    return $dir . '/' . $hashed_file_path;
   }
 
   private static function getFIleType(string $file_type): string
