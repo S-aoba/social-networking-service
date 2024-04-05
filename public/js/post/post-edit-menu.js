1b95772386608e43ef7f2626e5aa7aaa2dcd3b0e
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-04-05 19:09:37 +0900

    rm:unneeded code.

--- public/js/post/post-edit-menu.js
+++ public/js/post/post-edit-menu.js
@@ -1,17 +1,3 @@
-function toggleMenu(menu) {
-  // 対応するメニューを取得
-  var menuDiv = menu.querySelector('.menu');
-
-  // メニューの表示状態を切り替え
-  if (menuDiv.classList.contains('hidden')) {
-    // 非表示状態なら表示する
-    menuDiv.classList.remove('hidden');
-    menuDiv.classList.add('z-20');
-  } else {
-    // 表示状態なら非表示にする
-    menuDiv.classList.add('hidden');
-  }
-}
 const postMenus = document.querySelectorAll('#post-menu');
 
 postMenus.forEach((postMenu) => {
