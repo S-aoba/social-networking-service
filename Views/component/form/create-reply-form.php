cf4aa124ecc1782d8fb2c8ba584b223da654562a
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-04-08 22:32:16 +0900

    fix: reply form UI.

--- Views/component/form/create-reply-form.php
+++ Views/component/form/create-reply-form.php
@@ -1,34 +1,27 @@
 <?php
-$login_user_profile_image_path = is_null($login_user_profile->getUploadFullPathOfProfileImage()) ? '/images/default-icon.svg' : $login_user_profile->getUploadFullPathOfProfileImage();
+$profile_path = is_null($login_user_profile->getUploadFullPathOfProfileImage()) ? '/images/default-icon.svg' : $login_user_profile->getUploadFullPathOfProfileImage();
 
+$csrf_token = Helpers\CrossSiteForgeryProtection::getToken();
 ?>
 
-<div class="flex flex-col pt-3 pb-5 border-b border-slate-300">
-  <div class="w-full flex item-center px-3">
-    <div class="w-14 h-full">
-      <!-- ログインユーザーのプロフィールを持ってくる -->
-      <img src="<?= $login_user_profile_image_path ?>" alt="返信ユーザーアイコン" class="h-10 w-10 border border-slate-300 rounded-full">
-    </div>
-    <div class="w-full flex flex-col">
-      <form id="createReplyForm" method="post" enctype="multipart/form-data">
-        <input type="hidden" name="csrf_token" value="<?= Helpers\CrossSiteForgeryProtection::getToken(); ?>">
-        <div class="w-full flex items-center px-2">
-          <input type="hidden" name="post_id" value="<?= $data['post']->getId() ?>">
-          <textarea name="reply_content" id="reply_content" placeholder="返信をポスト" class="p-3 resize-none w-full min-h-14 focus:outline-none" maxlength="255" required></textarea>
-        </div>
-        <div class="w-full flex flex-col space-y-3 mt-2">
-          <label for="image" class="inline-block rounded-full cursor-pointer hover:bg-slate-100 h-9 w-9 p-2 transition-colors duration-300">
-            <img src="/images/upload-icon.svg" alt="upload-icon" class="object-fit w-full h-full">
-          </label>
-          <input id="image" name="image" type="file" accept=".png, .jpg, .jpeg, .gif, .mp4" class="hidden">
-          <div id="preview" class="max-h-96 w-full flex justify-center">
-          </div>
-          <div class="flex justify-end">
-            <button type="submit" class="px-4 py-2 border border-slate-300 rounded-md hover:bg-slate-200 transition-colors duration-300">返信</button>
-          </div>
-        </div>
-      </form>
-    </div>
+<div class="min-h-fit h-fit w-full flex border-b border-slate-200">
+  <div class="h-full min-w-18 w-20 flex justify-end py-5">
+    <img src="<?= htmlspecialchars($profile_path) ?>" alt="返信ユーザーアイコン" class="w-12 h-12 rounded-full border border-slate-200">
+  </div>
+  <div class="h-full w-full flex flex-col py-5">
+    <form id="createReplyForm" method="POST" enctype="multipart/form-data">
+      <input type="hidden" name="csrf_token" value="<?= $csrf_token ?>">
+      <input type="hidden" name="post_id" value="<?= $data['post']->getId() ?>">
+      <textarea id="reply_content" name="reply_content" class="pt-1 pb-3 px-3 resize-none w-full min-h-10 h-10 focus:outline-none" placeholder="返信をポスト" maxlength="255" required></textarea>
+      <div id="preview" class="max-h-96 w-full flex justify-center"></div>
+      <div class="flex justify-between mt-3 pr-3">
+        <label for="image" class="inline-block rounded-full cursor-pointer hover:bg-slate-100 h-9 w-9 p-2 transition-colors duration-300">
+          <img src="/images/upload-icon.svg" alt="upload-icon" class="object-fit w-full h-full">
+        </label>
+        <input id="image" name="image" type="file" accept=".png, .jpg, .jpeg, .gif, .mp4" class="hidden">
+        <button type="submit" class="px-4 py-2 border border-slate-300 rounded-md hover:bg-slate-200 transition-colors duration-300">投稿する</button>
+      </div>
+    </form>
   </div>
 </div>
 <script src="/js/preview.js"></script>
