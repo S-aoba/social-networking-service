82a181272deeec02e2541bb083366529b2552f72
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-04-08 22:40:05 +0900

    change: split presentation-tab component.

--- Views/page/home.php
+++ Views/page/home.php
@@ -1,18 +1,7 @@
 <div class="col-span-4 lg:col-span-3 min-h-screen h-screen w-full flex flex-col">
   <!-- トレンド and フォロワー タブ -->
   <?php if ($user) : ?>
-    <div class="grid grid-cols-2 min-h-14 h-14 w-full border-b border-slate-200 font-bold font-sans text-sm">
-      <div id="trend-tab" class="col-span-1 h-full flex items-center justify-center transition-colors duration-300 cursor-pointer hover:bg-slate-100">
-        <span class="h-full flex items-center px-3 <?= $presentationTab === 'trend' ? 'border-b-4 border-slate-600 relative top-0.5' : 'text-slate-400' ?>">
-          トレンド
-        </span>
-      </div>
-      <div id="follower-tab" class="col-span-1 h-full flex items-center justify-center transition-colors duration-300 cursor-pointer hover:bg-slate-100">
-        <span class="h-full flex items-center px-3 <?= $presentationTab === 'follower' ? 'border-b-4 border-slate-600 relative top-0.5' : 'text-slate-400' ?>">
-          フォロワー
-        </span>
-      </div>
-    </div>
+    <?php require 'Views/component/presentation-tab.php' ?>
     <?php require 'Views/component/form/create-post-form.php' ?>
   <?php endif; ?>
   <div class="w-full flex-grow overflow-auto">
