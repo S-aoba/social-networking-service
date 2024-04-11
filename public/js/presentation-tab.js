8f888427b25d121ce63cd87faca12290590a3f81
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-04-11 22:58:23 +0900

    add:if.

--- public/js/presentation-tab.js
+++ public/js/presentation-tab.js
@@ -1,14 +1,18 @@
 const trendTab = document.getElementById('trend-tab');
 const followerTab = document.getElementById('follower-tab');
 
-trendTab.addEventListener('click', () => {
-  document.cookie = 'presentation-tab=trend';
+if (trendTab) {
+  trendTab.addEventListener('click', () => {
+    document.cookie = 'presentation-tab=trend';
 
-  location.reload();
-});
+    location.reload();
+  });
+}
 
-followerTab.addEventListener('click', () => {
-  document.cookie = 'presentation-tab=follower';
+if (followerTab) {
+  followerTab.addEventListener('click', () => {
+    document.cookie = 'presentation-tab=follower';
 
-  location.reload();
-});
+    location.reload();
+  });
+}
--- public/js/preview.js
+++ public/js/preview.js
@@ -1,28 +1,31 @@
 const preview = document.getElementById('preview');
+const image = document.getElementById('image');
 
-document.getElementById('image').addEventListener('change', function () {
-  const file = this.files[0];
-  if (file) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      if (file.type.startsWith('video/')) {
-        // 動画の場合
-        const videoElement = document.createElement('video');
-        videoElement.setAttribute('id', 'preview-video');
-        videoElement.setAttribute('src', e.target.result);
-        videoElement.setAttribute('controls', ''); // controls属性を追加
-        preview.innerHTML = ''; // preview内の要素をクリア
-        preview.appendChild(videoElement);
-      } else {
-        // 画像の場合
-        const imageElement = document.createElement('img');
-        imageElement.setAttribute('id', 'preview-image');
-        imageElement.setAttribute('src', e.target.result);
-        imageElement.setAttribute('class', 'object-cover max-h-96');
-        preview.innerHTML = ''; // preview内の要素をクリア
-        preview.appendChild(imageElement);
-      }
-    };
-    reader.readAsDataURL(file);
-  }
-});
+if (preview && image) {
+  image.addEventListener('change', function () {
+    const file = this.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        if (file.type.startsWith('video/')) {
+          // 動画の場合
+          const videoElement = document.createElement('video');
+          videoElement.setAttribute('id', 'preview-video');
+          videoElement.setAttribute('src', e.target.result);
+          videoElement.setAttribute('controls', ''); // controls属性を追加
+          preview.innerHTML = ''; // preview内の要素をクリア
+          preview.appendChild(videoElement);
+        } else {
+          // 画像の場合
+          const imageElement = document.createElement('img');
+          imageElement.setAttribute('id', 'preview-image');
+          imageElement.setAttribute('src', e.target.result);
+          imageElement.setAttribute('class', 'object-cover max-h-96');
+          preview.innerHTML = ''; // preview内の要素をクリア
+          preview.appendChild(imageElement);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+}
