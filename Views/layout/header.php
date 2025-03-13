e85f64107d15bc515c9e8219cd3f645c1f82bb5c
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-03-13 20:58:57 +0900

    Setup tailwind css

--- Views/layout/header.php
+++ Views/layout/header.php
@@ -5,10 +5,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+        <!-- Tailwind CSS -->
+        <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 
         <title>Social Networking Service</title>
     </head>
     <body>
-        <main class="container mt-5 mb-5">
+        <main class="">
