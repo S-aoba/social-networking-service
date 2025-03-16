c2c4ea960d64ace5f5dc562e640f043fb65211f7
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-03-16 19:19:41 +0900

    Add Post Form component

---
+++ Views/component/post-form.php
@@ -0,0 +1,9 @@
+<div class="flex">
+  <div class="p-5">
+    <div class="size-10 rounded-full bg-sky-500/70"></div>
+  </div>
+  <textarea name="post" id="post-form" class="w-full resize-none p-2" placeholder="いまどうしてる？"></textarea>
+</div>
+<div class="w-full flex items-center justify-end my-2 pr-2">
+  <button class="py-2 px-4 rounded-4xl text-sm bg-gray-500/60 text-white font-semibold">ポストする</button>
+</div>
