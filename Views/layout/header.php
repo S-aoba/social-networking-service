fbfd6f1765b64c0826dff380e1f5e7f1fcad2921
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-03-16 17:21:55 +0900

    Update header layout

--- Views/layout/header.php
+++ Views/layout/header.php
@@ -10,5 +10,18 @@
 
         <title>Social Networking Service</title>
     </head>
-    <body class="h-screen w-full bg-white flex flex-col items-center">
-        <main class="h-full w-full">
+    <body class="h-screen w-screen flex flex-col bg-white">
+        <div class="flex justify-center w-full h-screen">
+            <div class="h-full flex max-w-screen-lg w-screen">
+                <div class="h-full w-60 flex flex-col border-r border-slate-200">
+                    <!-- Logo -->
+                    <?php include "Views/component/logo.php" ?>
+                    <div class="flex-1 p-4 flex flex-col items-center space-y-5">
+                        <!-- Navigation Item -->
+                        <?php include "Views/component/navigation-item.php"  ?>
+                    </div>
+                    <!-- User Information -->
+                    <?php include "Views/component/user-information.php" ?>
+                </div>
+                <main class="w-full h-full flex">
+
