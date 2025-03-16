1eb0c344f4ce35615e8e0c896d55f2fa86f97201
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-03-16 19:19:20 +0900

    Add TimeLine component

---
+++ Views/component/timeline.php
@@ -0,0 +1,18 @@
+<div class="max-w-[550px] w-full border-r border-slate-200">
+  <!-- TabList -->
+   <div class="w-full py-3 text-slate-400 text-sm border-b border-slate-200 flex">
+    <div class="w-1/2 text-center">
+      <p>おすすめ</p>
+    </div>
+    <div class="w-1/2 text-center">
+      <p>
+        フォロー中
+      </p>
+    </div>
+   </div>
+  <!-- Post Form -->
+   <?php include "Views/component/post-form.php"  ?>
+  <!-- Article -->
+   <?php include "Views/component/article.php"  ?>
+
+</div>
