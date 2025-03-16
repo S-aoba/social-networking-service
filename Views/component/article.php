262cede4b93ae26801df83397cdabfb4dccd1f81
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-03-16 19:19:30 +0900

    Add Article component

---
+++ Views/component/article.php
@@ -0,0 +1,16 @@
+<div class="border-t border-slate-200 divide-y divide-slate-200">
+  <div class="flex">
+    <div class="p-5">
+      <div class="size-10 bg-sky-500/70 rounded-full"></div>
+    </div>
+    <div class="p-2">
+      <div class="flex space-x-4">
+        <p class="text-sm font-semibold">蒼葉</p>
+        <p class="text-sm text-slate-400/60">@aoba__S</p>
+      </div>
+      <div class="mt-2">
+        <p class="text-sm">初めての投稿です。皆さんよろしくお願いします。初めての投稿です。皆さんよろしくお願いします。初めての投稿です。皆さんよろしくお願いします。初めての投稿です。皆さんよろしくお願いします。</p>
+      </div>
+    </div>
+  </div>
+</div>
