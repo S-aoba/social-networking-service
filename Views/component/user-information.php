d69d46a2be004d723640901e3dbca7b873bb680d
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-03-16 17:21:28 +0900

    Add user information component

---
+++ Views/component/user-information.php
@@ -0,0 +1,13 @@
+<div class="p-4">
+    <div class="w-full flex">
+        <div class="size-10 rounded-full bg-sky-500/70"></div>
+        <div class="pl-4">
+            <div>
+                <p class="text-base font-semibold">蒼葉</p>
+            </div>
+            <div>
+                <span class="text-sm text-slate-400/60">@aoba__S</span>
+            </div>
+        </div>
+    </div>
+</div>
