bdbb2188661702a3c7fe498dbf6a3ab7692cc5bf
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-03-18 17:11:33 +0900

    Delete css style

--- Views/page/login.php
+++ Views/page/login.php
@@ -1,18 +1,18 @@
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-6 offset-md-3">
-            <h2 class="text-center mb-4">Login</h2>
+<div>
+    <div>
+        <div>
+            <h2>Login</h2>
             <form action="form/login" method="post">
                 <input type="hidden" name="csrf_token" value="<?= Helpers\CrossSiteForgeryProtection::getToken(); ?>">
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="email" name="email" required>
+                <div -3">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" required>
                 </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="password" name="password" required>
+                <div -3">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" required>
                 </div>
-                <button type="submit" class="btn btn-primary">Login</button>
+                <button type="submit">Login</button>
             </form>
         </div>
     </div>
