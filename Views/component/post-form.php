8e31510a485ac9bd68628e15e6e8eb136c29db7c
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-03-19 15:49:56 +0900

    Change home and post-form UI

--- Views/component/post-form.php
+++ Views/component/post-form.php
@@ -5,5 +5,5 @@
   <textarea name="post" id="post-form" class="w-full resize-none p-2" placeholder="いまどうしてる？"></textarea>
 </div>
 <div class="w-full flex items-center justify-end my-2 pr-2">
-  <button class="py-2 px-4 rounded-4xl text-sm bg-gray-500/60 text-white font-semibold">ポストする</button>
+  <button type="submit" class="py-2 px-4 rounded-4xl text-sm bg-gray-500/60 text-white font-semibold hover:bg-slate-800 transition duration-300 focus:bg-slate-800">ポストする</button>
 </div>
