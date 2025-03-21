f0c9f596224f636c256f30ebbbbc15407166bc06
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-03-21 18:45:38 +0900

    Remove username handling from user creation method

--- Database/DataAccess/Implementations/UserDAOImpl.php
+++ Database/DataAccess/Implementations/UserDAOImpl.php
@@ -15,13 +15,12 @@ class UserDAOImpl implements UserDAO
 
         $mysqli = DatabaseManager::getMysqliConnection();
 
-        $query = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        $query = "INSERT INTO users (email, password) VALUES (?, ?)";
 
         $result = $mysqli->prepareAndExecute(
             $query,
-            'sss',
+            'ss',
             [
-                $user->getUsername(),
                 $user->getEmail(),
                 password_hash($password, PASSWORD_DEFAULT), // store the hashed password
             ]
