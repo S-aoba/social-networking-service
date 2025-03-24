03aec526ca665e4344542f9b984c56570c55923d
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-03-24 22:26:25 +0900

    Add follow method

--- Database/DataAccess/Implementations/FollowDAOImpl.php
+++ Database/DataAccess/Implementations/FollowDAOImpl.php
@@ -3,13 +3,22 @@
 namespace Database\DataAccess\Implementations;
 
 use Database\DataAccess\Interfaces\FollowDAO;
+use Database\DatabaseManager;
 use Models\Follow;
 
 class FollowDAOImpl implements FollowDAO
 {
   public function follow(int $userId, int $followUserId): bool
   {
-    return false;
+    $mysqli = DatabaseManager::getMysqliConnection();
+
+    $query = "INSERT INTO follows (follower_id, following_id) VALUES (?, ?)";
+
+    $result = $mysqli->prepareAndExecute($query, 'ii', [$userId, $followUserId]);
+
+    if(!$result) return false;
+    
+    return true;
   }
 
   public function unFollow(int $userId, int $followUserId): bool
