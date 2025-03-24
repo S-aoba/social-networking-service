e65a31e895e18906a62d58c90a032ca806343ceb
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-03-24 22:33:40 +0900

    Fix unFollow to unfollow

--- Database/DataAccess/Interfaces/FollowDAO.php
+++ Database/DataAccess/Interfaces/FollowDAO.php
@@ -7,7 +7,7 @@ use Models\Follow;
 interface FollowDAO
 {
   public function follow(int $userId, int $followUserId): bool;
-  public function unFollow(int $userId, int $followUserId): bool;
+  public function unfollow(int $userId, int $followUserId): bool;
   public function getFollowList($userId): ?Follow;
   public function getFollowerList($userId): ?Follow;
 }
