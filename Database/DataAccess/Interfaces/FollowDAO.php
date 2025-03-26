a22fda6f82b267deb261f0b32773346d289eeda8
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-03-26 10:49:39 +0900

    Fix getFollowList and getFollowerList method name

--- Database/DataAccess/Interfaces/FollowDAO.php
+++ Database/DataAccess/Interfaces/FollowDAO.php
@@ -8,6 +8,6 @@ interface FollowDAO
 {
   public function follow(int $userId, int $followUserId): bool;
   public function unfollow(int $userId, int $followUserId): bool;
-  public function getFollowList($userId): ?Follow;
   public function getFollowerList($userId): ?Follow;
+  public function getFollowingList($userId): ?Follow;
 }
