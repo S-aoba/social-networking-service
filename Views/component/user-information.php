dcce63f4e6ecb5a893432956b2f947bad1fbff42
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-03-29 19:24:46 +0900

    Add diplay follower count and following count

--- Views/component/user-information.php
+++ Views/component/user-information.php
@@ -7,9 +7,13 @@
         <div class="size-10 rounded-full overflow-hidden">
             <img src="<?php echo $imagePath ?>" alt="user-icon" class="w-full h-full object-cover">
         </div>
-        <div class="pl-4 flex items-center justify-center">
+        <div class="pl-4 flex flex-col items-start justify-center">
             <div>
                 <p class="text-base font-semibold"><?php echo $username ?></p>
+            </div>
+            <div class="text-sm">
+                <span><?php echo $followerCount ?> フォロー</span>
+                <span><?php echo $followingCount ?> フォロワー</span>
             </div>
         </div>
     </div>
