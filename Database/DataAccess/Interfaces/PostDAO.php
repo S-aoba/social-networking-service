53ae6f6fba7c15f4658696ef10856f16ad5f6b2f
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-03-29 19:03:33 +0900

    Change getById method name

--- Database/DataAccess/Interfaces/PostDAO.php
+++ Database/DataAccess/Interfaces/PostDAO.php
@@ -7,7 +7,7 @@ use Models\Post;
 interface PostDAO
 {
   public function create(Post $post): bool;
-  public function getById(int $id): ?Post;
   public function getByUserId(int $userId): ?Post;
+  public function getFollowingPosts(int $userId): ?array;
   // TODO: get post data by parentPostId
 }
