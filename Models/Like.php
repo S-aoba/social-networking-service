8dbe52a22e76debb29aaccf1e715c2c99fb20af7
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-01 22:24:15 +0900

    Add Like Model

---
+++ Models/Like.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Models;
+
+use Models\Interfaces\Model;
+use Models\Traits\GenericModel;
+
+class Like implements Model {
+    use GenericModel;
+
+    public function __construct(
+        private int $userId,
+        private int $postId,
+        private ?int $id = null,
+        private ?string $createdAt = null,
+    ) {}
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+    public function setId(int $id): void {
+        $this->id = $id;
+    }
+    public function getUserId(): int {
+        return $this->userId;
+    }
+    public function setUserId(int $userId): void {
+        $this->userId = $userId;
+    }
+    public function getPostId(): int {
+        return $this->postId;
+    }
+    public function setPostId(int $postId): void {
+        $this->postId = $postId;
+    } 
+    public function getCreatedAt(): ?string {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(string $createdAt): void {
+        $this->createdAt = $createdAt;
+    }
+}
