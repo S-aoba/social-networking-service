b85c59107db72a2e460196faaf9dc5dabbba7b56
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-01 22:32:26 +0900

    Add logic of createLike method

--- Database/DataAccess/Implementations/LikeDAOImpl.php
+++ Database/DataAccess/Implementations/LikeDAOImpl.php
@@ -4,11 +4,25 @@ namespace Database\DataAccess\Implementations;
 
 
 use Database\DataAccess\Interfaces\LikeDAO;
+use Database\DatabaseManager;
 use Models\Like;
 
 class LikeDAOImpl implements LikeDAO {
   public function createLike(Like $like): bool
   {
+    $mysqli = DatabaseManager::getMysqliConnection();
+
+    $query = "INSERT INTO likes (user_id, post_id) VALUES (?, ?)";
+
+    $result = $mysqli->prepareAndExecute($query, 'ii', [
+      $like->getUserId(),
+      $like->getPostId()
+    ]);
+
+    if (!$result) {
+      return false;
+    }
+
     return true;
   }
 
