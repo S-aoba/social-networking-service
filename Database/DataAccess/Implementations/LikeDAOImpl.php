75e45b9aaffaec930fda565ab47d8059f3b52015
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-01 22:24:32 +0900

    Add LikeDAOImpl class

---
+++ Database/DataAccess/Implementations/LikeDAOImpl.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\DataAccess\Implementations;
+
+use Database\DataAccess\Interfaces\Like;
+use Database\DataAccess\Interfaces\LikeDAO;
+
+class LikeDAOImpl implements LikeDAO {
+  public function createLike(Like $like): bool
+  {
+    return true;
+  }
+
+  public function deleteLike(int $id): bool
+  {
+    return true;
+  }
+
+  public function getLikesByUserId(int $userId): array
+  {
+    return [];
+  }
+
+  public function getLikesByPostId(int $postId): array
+  {
+    return [];
+  }
+
+  public function getAllLikes(): array
+  {
+    return [];
+  }
+}
