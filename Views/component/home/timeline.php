4bc6a1b06c71929988a7f2883a22eec9f1727ec7
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-03 21:33:13 +0900

    Fix timeline component height

--- Views/component/home/timeline.php
+++ Views/component/home/timeline.php
@@ -1,8 +1,8 @@
-<div class="col-span-8 w-full h-full">
+<div class="col-span-8 w-full h-full flex flex-col">
    <?php include "Views/component/home/tab.php" ?>
    <?php include "Views/component/post-form.php" ?>
 
-   <div class="w-full h-full">
+   <div class="w-full flex-1">
      <?php foreach ($followerPosts as $data): ?>
       <?php include "Views/component/article.php" ?>
      <?php endforeach; ?>
