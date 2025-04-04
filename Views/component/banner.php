fa0bb155dfc11b52d1b504c9e74fef00e8e36c53
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-04 13:39:10 +0900

    Refactor code

--- Views/component/banner.php
+++ Views/component/banner.php
@@ -1,16 +1,33 @@
 <?php 
 $navigationList = [
-    'Home' => '/images/home.svg',
-    '通知' => '/images/notification.svg',
-    'メッセージ' => '/images/message.svg',
-    'プロフィール' => '/images/profile.svg'
+    [
+        'label' => 'Home',
+        'imagePath' => '/images/home.svg',
+        'linkPath' => '/'
+    ],
+    [
+        'label' => '通知',
+        'imagePath' => '/images/notification.svg',
+        'linkPath' => '#'
+
+    ],
+    [
+        'label' => 'メッセージ',
+        'imagePath' => '/images/message.svg',
+        'linkPath' => '#'
+    ],
+    [
+        'label' => 'プロフィール',
+        'imagePath' => '/images/profile.svg',
+        'linkPath' => "/profile?user={$userId}"
+    ]
 ];
 ?>
 
 <div class="col-span-2 w-full h-full flex flex-col border-r border-slate-200">
     <?php include "Views/component/logo.php"; ?>
     <div class="w-full flex-1 py-5 flex flex-col">
-        <?php foreach ($navigationList as $label => $iconPath): ?>
+        <?php foreach ($navigationList as $item): ?>
             <?php include "Views/component/navigation-item.php" ?>
         <?php endforeach; ?>
     </div>
--- Views/component/navigation-item.php
+++ Views/component/navigation-item.php
@@ -1,9 +1,9 @@
-<a href="/profile?user=1" class="w-full h-fit">
+<a href="<?php echo htmlspecialchars($item['linkPath'], ENT_QUOTES, 'UTF-8') ?>" class="w-full h-fit">
     <div class="w-fit p-3 flex space-x-2 justify-start items-center rounded-3xl hover:cursor-pointer hover:bg-stone-300/45 transition duration-300">
-        <img src="<?php echo htmlspecialchars($iconPath, ENT_QUOTES, 'UTF-8'); ?>" 
-             alt="<?php echo htmlspecialchars($label, ENT_QUOTES, 'UTF-8'); ?>-icon" 
+        <img src="<?php echo htmlspecialchars($item['imagePath'], ENT_QUOTES, 'UTF-8'); ?>" 
+             alt="<?php echo htmlspecialchars($item['label'], ENT_QUOTES, 'UTF-8'); ?>-icon" 
              class="w-6 h-6 text-black"
         >
-        <p class="font-semibold"><?php echo htmlspecialchars($label, ENT_QUOTES, 'UTF-8'); ?></p>
+        <p class="font-semibold"><?php echo htmlspecialchars($item['label'], ENT_QUOTES, 'UTF-8'); ?></p>
     </div>
 </a>
