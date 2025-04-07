2b5c49864a50434a5c53ff2e36ec3c0858a4f4fb
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-07 14:10:45 +0900

    Add getById method

--- Database/DataAccess/Interfaces/PostDAO.php
+++ Database/DataAccess/Interfaces/PostDAO.php
@@ -7,6 +7,7 @@ use Models\Post;
 interface PostDAO
 {
   public function create(Post $post): bool;
+  public function getById(int $postId): ?array;
   public function getByUserId(int $userId): ?array;
   public function getFollowingPosts(int $userId): ?array;
   public function deletePost(int $postId): bool;
