de6dc6254537c284d845700b60b20595b0838349
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-07 14:29:21 +0900

    Add post detail ui, only post data

---
+++ Views/component/post/post-detail.php
@@ -0,0 +1,9 @@
+<div class="col-span-8 w-full h-full flex flex-col">
+  <div class="flex items-center gap-x-2 p-2">
+    <img src="/images/undo-icon.svg" alt="undo-icon" class="size-5">
+    <span class="font-semibold">ポスト</span>
+  </div>
+  <div class="w-full h-fit border-b border-slate-200">
+    <?php include "Views/component/article.php" ?>
+  </div>
+</div>
