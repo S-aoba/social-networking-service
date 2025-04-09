641f651fb8069d378a1aa82eeff7f7f17b52a4fe
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-09 22:28:14 +0900

    Add following data

--- Views/component/followingList.php
+++ Views/component/followingList.php
@@ -2,19 +2,18 @@
   <!-- ヘッダー -->
   <div class="flex items-center space-x-2">
     <img src="/images/undo-icon.svg" alt="undo-icon" class="w-5 h-5">
-    <div class="text-xl font-semibold">Usernameのフォロワー</div>
+    <div class="text-xl font-semibold"><?= $username ?></div>
   </div>
 
-  <!-- フォロワー一覧 -->
   <div class="flex flex-col space-y-4">
-    <!-- フォロワー1 -->
+    <?php foreach($data as $item): ?>
     <div class="flex items-start space-x-4 p-4">
       <img src="/images/default-icon.png" alt="user-icon" class="w-12 h-12 rounded-full">
       <div class="flex flex-col space-y-1">
-        <div class="font-medium text-lg">username1</div>
-        <div class="text-sm text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-        <div class="text-xs text-blue-600 font-semibold">フォロー中</div>
+        <div class="font-medium text-lg"><?= $item->getUsername() ?></div>
+        <div class="text-sm text-gray-600"><?= $item->getSelfIntroduction(); ?></div>
       </div>
     </div>
+    <?php endforeach; ?>
   </div>
 </div>
