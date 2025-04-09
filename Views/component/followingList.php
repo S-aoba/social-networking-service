d03ac3963d80e5a93e4ab5143c84c0638abafbbe
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-09 22:07:59 +0900

    Add prototype of following UI

---
+++ Views/component/followingList.php
@@ -0,0 +1,20 @@
+<div class="col-span-8 w-full h-full flex flex-col space-y-4 p-4 bg-white rounded-xl shadow-md">
+  <!-- ヘッダー -->
+  <div class="flex items-center space-x-2">
+    <img src="/images/undo-icon.svg" alt="undo-icon" class="w-5 h-5">
+    <div class="text-xl font-semibold">Usernameのフォロワー</div>
+  </div>
+
+  <!-- フォロワー一覧 -->
+  <div class="flex flex-col space-y-4">
+    <!-- フォロワー1 -->
+    <div class="flex items-start space-x-4 p-4">
+      <img src="/images/default-icon.png" alt="user-icon" class="w-12 h-12 rounded-full">
+      <div class="flex flex-col space-y-1">
+        <div class="font-medium text-lg">username1</div>
+        <div class="text-sm text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+        <div class="text-xs text-blue-600 font-semibold">フォロー中</div>
+      </div>
+    </div>
+  </div>
+</div>
