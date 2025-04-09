a732326e31672b368651d7204a9c4acc5591b788
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-09 16:29:26 +0900

    Add dropdown component for user information

--- Views/component/user-information.php
+++ Views/component/user-information.php
@@ -2,11 +2,7 @@
     $imagePath = $imagePath === null ? '/images/default-icon.png' : $imagePath;
 ?>
 
+
 <div class="max-h-24 h-full flex">
-    <div class="flex items-center justify-center">
-        <img src="<?php echo $imagePath ?>" alt="user-icon" class="size-10">
-    </div>
-    <div class="w-fit h-full flex items-center justify-start pl-2">
-        <p class="font-semibold"><?php echo $username ?></p>
-    </div>
+    <?php include 'Views/component/dropdown.php' ?>
 </div>
