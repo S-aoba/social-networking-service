7e5c63f8428d668438fb264a6ecf49468e0e7957
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-13 15:12:27 +0900

    Remove code because these are unneeded

--- Database/DataAccess/Interfaces/LikeDAO.php
+++ Database/DataAccess/Interfaces/LikeDAO.php
@@ -9,10 +9,4 @@ interface LikeDAO
     public function createLike(Like $like): bool;
 
     public function unlike(Like $like): bool;
-
-    public function getLikesByUserId(int $userId): array;
-
-    public function getLikesByPostId(int $postId): array;
-
-    public function getAllLikes(): array;
 }
