b3d9a402bc46bcce3e1c3c7bc418994e8a1ac2aa
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-15 10:50:18 +0900

    Removed the image path code so it moved to the dropdown component

--- Views/component/user-information.php
+++ Views/component/user-information.php
@@ -1,8 +1,3 @@
-<?php
-    $imagePath = $imagePath === null ? '/images/default-icon.png' : $imagePath;
-?>
-
-
 <div class="max-h-24 h-full flex">
     <?php include 'Views/component/dropdown.php' ?>
 </div>
