a791e811ecc2ece564be6dcff2b79bbfc6796a1d
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-21 16:02:28 +0900

    Change user icon's round in the user incformation component

--- Views/component/dropdown.php
+++ Views/component/dropdown.php
@@ -6,7 +6,7 @@
   <div>
     <button  type="button" class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-200 ring-inset hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
       <div class="flex items-center justify-center space-x-2">
-        <img src="<?php echo $imagePath ?>" alt="user-icon" class="size-8">
+        <img src="<?php echo $imagePath ?>" alt="user-icon" class="size-8 rounded-full">
         <span class="text-xs font-semibold"><?php echo $profile->getUsername() ?></span>
         <svg class="-mr-1 size-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
           <path fill-rule="evenodd" d="M14.78 11.78a.75.75 0 0 1-1.06 0L10 8.06l-3.72 3.72a.75.75 0 1 1-1.06-1.06l4.25-4.25a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06z" clip-rule="evenodd" />
