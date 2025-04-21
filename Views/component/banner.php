8a48132ec3cba95771ba09d3cc3d36fc707fd0f4
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-21 13:34:12 +0900

    Change method name

--- Views/component/banner.php
+++ Views/component/banner.php
@@ -19,7 +19,7 @@ $navigationList = [
     [
         'label' => 'プロフィール',
         'imagePath' => '/images/profile.svg',
-        'linkPath' => "/profile?user={$profile->getUserName()}"
+        'linkPath' => "/profile?user={$profile->getUsername()}"
     ]
 ];
 ?>
