bf24deec9a1a0deef457504a065954fb2d2f54ce
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-21 17:29:49 +0900

    Change height of post form textarea

--- Views/component/post-form.php
+++ Views/component/post-form.php
@@ -14,14 +14,18 @@
     <input type="hidden" name="csrf_token" value="<?= Helpers\CrossSiteForgeryProtection::getToken() ?>">
     <input type="hidden" name="parent_post_id" value="">
 
-    <textarea name="content" id="content" class="w-full resize-none p-2 focus:outline-none" placeholder="いまどうしてる？"></textarea>
+    <textarea 
+      id="content" 
+      name="content" 
+      class="w-full h-40 resize-none p-2 focus:outline-none" 
+      placeholder="いまどうしてる？"
+    ></textarea>
     <div class="w-full h-full">
       <div id="preview-container"></div>
       <label for="upload-file" class="hover:cursor-pointer">
         <img src="/images/camera.svg" alt="upload-icon">
         <input type="file" id="upload-file" name="upload-file" class="hidden" value="">
       </label>
-
     </div>
     <div class="w-full flex items-center justify-end my-2 pr-2">
       <button type="submit" class="py-2 px-4 rounded-4xl text-sm bg-gray-500/60 text-white font-semibold hover:bg-slate-800 transition duration-300 focus:bg-slate-800">ポストする</button>
