7573a99feb4566ad7267ff96ecb283a22062fd08
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-25 22:05:53 +0900

    Separated getPublicImagePath method for profile and post

--- Models/ImageService.php
+++ Models/ImageService.php
@@ -31,7 +31,15 @@ class ImageService implements Model {
       return $this->convertToFullImagePath();
     }
 
-    public function getPublicImagePath(string $imagePath): string {
+    public function getPublicProfileImagePath(?string $imagePath): string {
+      if($imagePath === null) return '/images/default-icon.png';
+
+      return '/' . $this->dirPath . '/' . $imagePath;
+    }
+
+    public function getPublicPostImagePath(?string $imagePath): ?string {
+      if($imagePath === null) return null;
+
       return '/' . $this->dirPath . '/' . $imagePath;
     }
 
