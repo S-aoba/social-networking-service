f8a4c73a83d1f374da8d3de9b11d1ac158ce7643
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-04-30 12:14:27 +0900

    Add the link and change list UI

--- Views/component/followList.php
+++ Views/component/followList.php
@@ -4,9 +4,10 @@
     <div class="text-xl font-semibold"><?= $authUser->getUsername(); ?></div>
   </div>
 
-  <div class="flex flex-col space-y-4">
+  <div class="flex flex-col divide-y divide-slate-200">
     <?php foreach($data as $item): ?>
-    <div class="flex items-start space-x-4 p-4">
+    <div class="relative flex items-start space-x-4 p-4">
+      <a href="/profile?user=<?= $item->getUsername(); ?>" class="absolute inset-0 hover:bg-slate-400 opacity-20 transition duration-300"></a>
       <img src="<?= $item->getImagePath(); ?>" alt="user-icon" class="w-12 h-12 rounded-full">
       <div class="flex flex-col space-y-1">
         <div class="font-medium text-lg"><?= $item->getUsername() ?></div>
