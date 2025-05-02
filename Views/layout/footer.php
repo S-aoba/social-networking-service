05c6720bc91ef729d2cc1c5b4d67e1f74919cf83
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-02 13:52:50 +0900

    Remove footer UI

--- Views/layout/footer.php
+++ Views/layout/footer.php
@@ -1,10 +1,3 @@
         </main>
-
-        <footer class="text-center py-4 bg-gray-200 w-full">
-            <div>
-                © 2025:
-                <a href="/">SocialNetworkingService.com</a>
-            </div>
-        </footer>
     </body>
 </html>
