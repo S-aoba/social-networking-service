e29ff3c6666786ef9a89cf5a48d7c51864f903ea
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-04 18:58:48 +0900

    Add conversation dao class

---
+++ Database/DataAccess/Interfaces/ConversationDAO.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Database\DataAccess\Interfaces;
+
+use Models\Conversation;
+
+interface ConversationDAO {
+  public function create(Conversation $conversation): bool;
+  public function findAllByUserId(int $userId): ?array;
+  public function delete(int $id): bool;
+}
