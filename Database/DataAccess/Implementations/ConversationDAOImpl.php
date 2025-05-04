53917aa0560b6b0427ae5b6b9f9e5a3cbf6da576
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-04 21:49:10 +0900

    Add a logic of create and existsByUesrPair method

--- Database/DataAccess/Implementations/ConversationDAOImpl.php
+++ Database/DataAccess/Implementations/ConversationDAOImpl.php
@@ -3,12 +3,29 @@
 namespace Database\DataAccess\Implementations;
 
 use Database\DataAccess\Interfaces\ConversationDAO;
+use Database\DatabaseManager;
+
 use Models\Conversation;
 
 class ConversationDAOImpl implements ConversationDAO {
   public function create(Conversation $conversation): bool
   {
-    return false;    
+    if($conversation->getId() !== null) throw new \Exception('Cannot create a conversation with an existing ID. id: ' . $conversation->getId());
+    
+    $mysqli = DatabaseManager::getMysqliConnection();
+
+    $query = "INSERT INTO conversations (user1_id, user2_id) VALUES (?, ?)";
+
+    $result = $mysqli->prepareAndExecute($query, 'ii', [
+      $conversation->getUser1Id(),
+      $conversation->getUser2Id()
+    ]);
+
+    if($result === false) return false;
+
+    $conversation->setId($mysqli->insert_id);
+    
+    return true;    
   }
 
   public function findAllByUserId(int $userId): ?array
@@ -16,8 +33,37 @@ class ConversationDAOImpl implements ConversationDAO {
     return null;
   }
 
+  public function existsByUserPair(Conversation $conversation): bool
+  {
+    $conversationRowData = $this->existsRowByUserPair($conversation);
+
+    if($conversationRowData === false) return false;
+
+    return true;
+  }
+
   public function delete(int $id): bool
   {
     return false;
+  }
+
+  private function existsRowByUserPair(Conversation $conversation): bool {
+    $mysqli = DatabaseManager::getMysqliConnection();
+
+    $query = "SELECT 1
+              FROM conversations
+              WHERE (user1_id = ? AND user2_id = ?)
+                 OR (user1_id = ? AND user2_id = ?)
+              LIMIT 1
+            ";
+    
+    $result = $mysqli->prepareAndFetchAll($query, 'iiii', [
+      $conversation->getUser1Id(),
+      $conversation->getUser2Id(),
+      $conversation->getUser2Id(),
+      $conversation->getUser1Id(),
+    ]);
+
+    return !empty($result);
   }
 }
