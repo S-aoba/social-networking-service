e40e28e51abccddd74694c5ac757eff734062317
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-04 21:48:20 +0900

    Add existsByUserPair method

--- Database/DataAccess/Interfaces/ConversationDAO.php
+++ Database/DataAccess/Interfaces/ConversationDAO.php
@@ -7,5 +7,6 @@ use Models\Conversation;
 interface ConversationDAO {
   public function create(Conversation $conversation): bool;
   public function findAllByUserId(int $userId): ?array;
+  public function existsByUserPair(Conversation $conversation): bool;
   public function delete(int $id): bool;
 }
