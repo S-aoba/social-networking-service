45f68e2f9d459abc95ced1c89f81df4d076910fa
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-04 17:32:13 +0900

    Add conversations table

---
+++ Database/Migrations/2025-05-04_1746347133_CreateConversationTable.php
@@ -0,0 +1,32 @@
+<?php
+namespace Database\Migrations;
+
+use Database\SchemaMigration;
+
+class CreateConversationTable implements SchemaMigration
+{
+    public function up(): array
+    {
+        // マイグレーションロジックをここに追加してください
+        return [
+            "CREATE TABLE conversations (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                user1_id BIGINT NOT NULL,
+                user2_id BIGINT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+                FOREIGN KEY (user1_id) REFERENCES users(id),
+                FOREIGN KEY (user2_id) REFERENCES users(id)
+            )"
+        ];
+    }
+
+    public function down(): array
+    {
+        // ロールバックロジックを追加してください
+        return [
+            "DROP TABLE conversations"
+        ];
+    }
+}
