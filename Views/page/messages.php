cb04a78301fccc112a7494fe1e3c1128be653d99
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-07 21:20:27 +0900

    Add conversations data

--- Views/page/messages.php
+++ Views/page/messages.php
@@ -20,25 +20,34 @@
     </div>
     <!-- DM Message List -->
       <div class="flex-1 py-4">
-      <?php for($i = 0; $i < 5; $i++): ?>
+      <?php foreach($conversations as $data): ?>
         <!-- DM Message List Item -->
         <div class="flex p-4 cursor-pointer transition duration-300 hover:bg-slate-100">
           <!-- User icon -->
             <div class="w-12 shrink-0">
-              <img src="images/default-icon.png" alt="user-icon" class="size-10">
+              <img src="<?= $data['partner']->getImagePath(); ?>" alt="user-icon" class="size-10 rounded-full">
             </div>
           <!-- User info and message -->
           <div class="flex-1 min-w-0">
             <div class="flex items-center space-x-4 text-sm pb-2">
-              <p class="font-semibold">aoba</p>
-              <span class="text-slate-400">3時間前</span>
+              <p class="font-semibold"><?= $data['partner']->getUsername(); ?></p>
+              <span class="text-slate-400">
+                <?= $data['directMessage'] === null ? 
+                      '3時間前' 
+                      :  
+                      $data['directMessage']->getCreatedAt()?>
+              </span>
             </div>
-            <div class="text-sm">
-              <p class="truncate overflow-hidden whitespace-nowrap text-ellipsis">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error modi facere ut! Ipsa, quod? Blanditiis sed beatae, repellendus doloribus dolor eius praesentium odio enim. Quia numquam veniam repellat deserunt nulla.</p>
-            </div>
+            <?php if($data['directMessage'] !== null): ?>
+              <div class="text-sm">
+                <p class="truncate overflow-hidden whitespace-nowrap text-ellipsis">
+                  <?= $data['directMessage']->getContent(); ?>
+                </p>
+              </div>
+            <?php endif; ?>
           </div>
         </div>
-      <?php endfor; ?>
+      <?php endforeach; ?>
       </div>
   </div>
   <?php include "Views/component/dm/dm-message-preview.php" ?>
