82c39a818ec334aca5292199f54553ed5b6b9e63
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-07 14:04:23 +0900

    Add the function to create the direct message

--- Database/DataAccess/Implementations/DirectMessageDAOImpl.php
+++ Database/DataAccess/Implementations/DirectMessageDAOImpl.php
@@ -4,12 +4,29 @@ namespace Database\DataAccess\Implementations;
 
 use Database\DataAccess\Interfaces\DirectMessageDAO;
 use Database\DatabaseManager;
+
 use Models\DirectMessge;
 
 class DirectMessageDAOImpl implements DirectMessageDAO {
   public function create(DirectMessge $directMessage): bool
   {
-    return false;
+    if ($directMessage->getId() !== null) throw new \Exception('Cannot create a direct-message with an existing ID. id: ' . $directMessage->getId());
+
+    $mysqli = DatabaseManager::getMysqliConnection();
+
+    $query = "INSERT INTO direct_messages (conversation_id, sender_id, content) VALUES (?, ?, ?)";
+
+    $result = $mysqli->prepareAndExecute($query, 'iis', [
+      $directMessage->getConversationId(),
+      $directMessage->getSenderId(),
+      $directMessage->getContent()
+    ]);
+
+    if($result === false) return false;
+
+    $directMessage->setId($mysqli->insert_id);
+
+    return true;
   }
   
   public function findByConversationId(int $conversationId): ?array
