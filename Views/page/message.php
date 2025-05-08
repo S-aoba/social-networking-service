3fdbcff942cb29187a9e1c4e6bdda19c0113abfc
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-08 13:35:47 +0900

    Add direct message data in the message page

--- Views/page/message.php
+++ Views/page/message.php
@@ -51,6 +51,46 @@
           </a>
         <?php endforeach; ?>
       </div>
-  </div>
-  <!-- 実際のメッセージを表示する部分 -->
+      <!-- Direct Message -->
+    </div>
+    <div class="col-span-6">
+      <div class="px-4 py-2">
+        <span class="font-semibold"><?= $data['partner']->getUsername(); ?></span>        
+      </div>
+      <div class="pb-10 flex flex-col items-center justify-center border-b border-slate-100">
+        <div class="flex flex-col items-center justify-center space-y-2">
+          <img src="<?= $data['partner']->getImagePath(); ?>" alt="partner-user-icon">
+          <span class="font-semibold"><?= $data['partner']->getUsername(); ?></span>
+          <p class="text-xs text-slate-600">2021年11月からSNSを利用しています</p>
+        </div>
+      </div>
+      <!-- Direct Messages -->
+      <div>
+        <?php foreach($directMessages as $m): ?>
+          <?php if($authUser->getUserId() === $m->getSenderId()): ?>
+            <!-- AuthUser -->
+            <div class="p-3 flex flex-col items-end justify-center space-y-2">
+              <span class="font-semibold">
+                <?= $authUser->getUsername(); ?>
+              </span>
+              <div class="p-4 bg-slate-100 rounded-3xl mb-2">
+                <p><?= $m->getContent(); ?></p>
+              </div>
+              <span class="text-xs text-slate-400"><?= $m->getCreatedAt(); ?></span>
+            </div>
+          <?php else: ?>
+            <!-- PartnerUser -->
+            <div class="p-3 flex flex-col items-start justify-center space-y-2">
+              <span class="font-semibold">
+                <?= $data['partner']->getUsername(); ?>
+              </span>
+              <div class="p-4 bg-slate-100 rounded-3xl mb-2">
+                <p><?= $m->getContent(); ?></p>
+              </div>
+              <span class="text-xs text-slate-400"><?= $m->getCreatedAt(); ?></span>
+            </div>
+          <?php endif; ?>
+        <?php endforeach; ?>
+      </div>
+    </div>
 </div>
