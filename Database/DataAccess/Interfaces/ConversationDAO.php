7d2b438784336a411f97d50e76cf62eb611e5bb2
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-09 17:48:00 +0900

    Fix return type of findByConversationId method in ConversationDAO interface

--- Database/DataAccess/Interfaces/ConversationDAO.php
+++ Database/DataAccess/Interfaces/ConversationDAO.php
@@ -7,7 +7,7 @@ use Models\Conversation;
 interface ConversationDAO {
   public function create(Conversation $conversation): bool;
   public function findAllByUserId(int $userId): ?array;
-  public function findByConversationId(int $id): ?array;
+  public function findByConversationId(int $id): ?Conversation;
   public function existsByUserPair(Conversation $conversation): bool;
   public function delete(int $id): bool;
 }
