0738489e32028532673d87374dea16887cb0380b
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-12 16:41:54 +0900

    Adjusted form submit button style, changed message input to text area and added auto-resize feature

--- Views/page/message.php
+++ Views/page/message.php
@@ -102,20 +102,20 @@
 
     <!-- Direct Message Form -->
     <div class="bg-white py-4 px-2 border-t border-slate-200">
-      <form action="form/direct-message" method="POST" class="relative flex space-x-2">
+      <form action="form/direct-message" method="POST" class="relative">
         <input type="hidden" name="csrf_token" value="<?= Helpers\CrossSiteForgeryProtection::getToken
         () ?>">
         <input type="hidden" name="conversation_id" value="<?= $conversation->getId(); ?>">
         <input type="hidden" name="sender_id" value="<?= $authUser->getUserId(); ?>">        
         <textarea 
-            name="content" 
-            class="w-full resize-none h-10 bg-slate-200 text-sm p-2 border border-slate-200 rounded-md focus:outline-none" 
-            placeholder="新しいメッセージを作成"
-          >
-        </textarea>
+          id="content"
+          name="content" 
+          class="w-full resize-none h-10 bg-slate-200 text-sm p-2 border border-slate-200 rounded-md focus:outline-none" 
+          placeholder="新しいメッセージを作成"
+          ></textarea>
         <button 
           type="submit" 
-          class="absolute right-0 px-4 py-2 rounded-md focus:outline-none"
+          class="absolute right-2 px-4 py-2 rounded-md focus:outline-none"
         >
           <img 
             src="images/send-icon.svg" 
@@ -129,4 +129,19 @@
 
 </div>
 
+<script>
+  window.addEventListener('DOMContentLoaded', () => {
+    const textarea = document.getElementById('content');
+
+    const defaultHeight = '40';
+
+    const autoResize = () => {
+    textarea.style.height = '40px';
+    const scrollHeight = textarea.scrollHeight;
+    textarea.style.height = Math.max(scrollHeight, defaultHeight) + 'px';
+  };
+
+    textarea.addEventListener('input', autoResize);
+  })
+</script>
 <script src="js/open-create-conversation-modal.js"></script>
