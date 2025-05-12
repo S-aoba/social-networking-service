7bcd1e8cc259dba6ebe44da7cbc828d5c6cef2e9
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-12 22:11:28 +0900

    Add dm-search-bar component

--- Views/page/messages.php
+++ Views/page/messages.php
@@ -17,10 +17,7 @@
     </div>
 
     <!-- DM Search bar -->
-    <div class="relative pt-2 px-4">
-      <img src="images/search-icon.svg" alt="search-icon" class="absolute top-5 left-7 size-5">
-      <input type="text" class="size-full text-sm border border-slate-300 rounded-3xl py-2.5 pl-8 placeholder-slate-600 focus:outline-none" placeholder="ダイレクトメッセージを検索">
-    </div>
+    <?php include "Views/component/dm/search-bar.php" ?>
 
     <!-- Conversations -->
     <div class="flex-1 py-4">
