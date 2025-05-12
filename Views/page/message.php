ec1b495d57b8424ada6dca91301fedb8d98891b3
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-12 22:12:51 +0900

    Add dm-header component

--- Views/page/message.php
+++ Views/page/message.php
@@ -4,17 +4,7 @@
   <div class="col-span-4 w-full h-full flex flex-col overflow-auto border-r border-slate-200">
 
     <!-- header -->
-    <div class="flex items-center justify-between py-2 px-4">
-        <h2 class="text-xl font-semibold">メッセージ</h2>
-        <div class="flex">
-          <div class="flex items-center justify-center size-8 hover:bg-gray-400/30 rounded-full cursor-not-allowed transition duration-300">
-            <img src="images/settings-icon.svg" alt="settings-icon" class="size-5">
-          </div>
-          <div class="compose-conversation-button flex items-center justify-center size-8 hover:bg-gray-400/30 rounded-full transition duration-300 cursor-pointer">
-            <img src="images/compose-icon.svg" alt="compose-icon" class="size-5">
-          </div>
-        </div>
-    </div>
+    <?php include "Views/component/dm/header.php" ?>
 
     <!-- DM Search bar -->
     <?php include "Views/component/dm/search-bar.php" ?>
