6be597563720cfc3e3c80f9fcad942b6a526f6f0
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-14 22:54:19 +0900

    Change constructor and add a method of toArray

--- Models/File.php
+++ Models/File.php
@@ -9,32 +9,37 @@ class File implements Model {
   use GenericModel;
 
   public function __construct(
-    private array $file
+    private string $name,
+    private string $fullPath,
+    private string $type,
+    private string $tmpName,
+    private int $error,
+    private int $size
   )
   {}
 
   public function getName(): ?string {
-    return $this->file['name'] === '' ? null : $this->file['name'];
+    return $this->name === '' ? null : $this->name;
   }
 
   public function getFullPath(): ?string {
-    return $this->file['full_path'] === '' ? null : $this->file['full_path'];
+    return $this->fullPath === '' ? null : $this->fullPath;
   }
 
   public function getType(): ?string {
-    return $this->file['type'] === '' ? null : $this->file['type'];
+    return $this->type === '' ? null : $this->type;
   }
 
   public function getTmpName(): ?string {
-    return $this->file['tmp_name'] === '' ? null : $this->file['tmp_name'];
+    return $this->tmpName === '' ? null : $this->tmpName;
   }
 
   public function getError(): int {
-    return $this->file['error'];
+    return $this->error;
   }
 
   public function getSize(): int {
-    return $this->file['size'];
+    return $this->size;
   }
 
   public function isValid(): bool {
@@ -42,5 +47,16 @@ class File implements Model {
            !empty($this->getTmpName()) &&
            is_uploaded_file($this->getTmpName()) &&
            $this->getSize() > 0;
+  }
+
+  public static function fromArray(array $file): self {
+    return new self(
+        name: $file['name'] ?? '',
+        fullPath: $file['full_path'] ?? '',
+        type: $file['type'] ?? '',
+        tmpName: $file['tmp_name'] ?? '',
+        error: $file['error'] ?? UPLOAD_ERR_NO_FILE,
+        size: $file['size'] ?? 0
+    );
   }
 }
