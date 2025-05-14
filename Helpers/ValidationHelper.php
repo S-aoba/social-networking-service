f8ae69bb2279f72beb621a1151d48293207292e2
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-14 11:41:07 +0900

    Remove unused code

--- Helpers/ValidationHelper.php
+++ Helpers/ValidationHelper.php
@@ -87,17 +87,7 @@ class ValidationHelper
                 ValueType::STRING => self::string($value),
                 ValueType::INT => self::integer($value), 
                 ValueType::FLOAT => filter_var($value, FILTER_VALIDATE_FLOAT),
-                ValueType::DATE => self::validateDate($value),
-                ValueType::EMAIL => filter_var($value, FILTER_VALIDATE_EMAIL),
-                ValueType::PASSWORD =>
-                                    is_string($value) &&
-                                    strlen($value) >= 8 && // Minimum 8 characters
-                                    preg_match('/[A-Z]/', $value) && // 少なくとも1文字の大文字
-                                    preg_match('/[a-z]/', $value) && // 少なくとも1文字の小文字
-                                    preg_match('/\d/', $value) && // 少なくとも1桁
-                                    preg_match('/[\W_]/', $value) // 少なくとも1つの特殊文字（アルファベット以外の文字）
-                                        ? $value : throw new \InvalidArgumentException("The provided value is not a valid password."),
-                default => throw new \InvalidArgumentException(sprintf("Invalid type for field: %s, with type %s", $field, $type)),
+                ValueType::DATE => self::validateDate($value)
             };
 
             if ($validatedValue === false) {
