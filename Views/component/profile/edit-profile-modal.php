43ab43b40a6869c4309b42f4f22ea76910d3426c
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-15 22:10:33 +0900

    Refactor UI of edit profile modal component

--- Views/component/profile/edit-profile-modal.php
+++ Views/component/profile/edit-profile-modal.php
@@ -1,59 +1,145 @@
-<div id="modal" class="relative z-50 hidden" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-  <div class="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true"></div>
+<?php
+$profileInfoList = [
+  'name' => [
+    'label' => '名前',
+    'name' => 'username',
+    'type' => 'text',
+    'value' => $queryUser->getUsername()
+  ],
+  'age' => [
+    'label' => '年齢',
+    'name' => 'age',
+    'type' => 'number',
+    'value' => $queryUser->getAge()
+  ],
+  'address' => [
+    'label' => '住所',
+    'name' => 'address',
+    'type' => 'text',
+    'value' => $queryUser->getAddress()
+  ],
+  'hobby' => [
+    'label' => '趣味',
+    'name' => 'hobby',
+    'type' => 'text',
+    'value' => $queryUser->getHobby()
+  ],
+  'selfIntroduction' => [
+    'label' => '自己紹介',
+    'name' => 'self_introduction',
+    'rows' => 4,
+    'value' => $queryUser->getSelfIntroduction()
+  ],
+];
+?>
+
+
+
+<div 
+  id="modal" 
+  role="dialog" 
+  class="relative z-50 hidden" 
+  aria-labelledby="modal-title" 
+  aria-modal="true"
+>
+  <div 
+    class="fixed inset-0 bg-gray-500/75 transition-opacity" 
+    aria-hidden="true"
+  ></div>
   
-  <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-    <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-      <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-          <form id="editProfileForm" method="POST" action="/form/update/profile" enctype="multipart/form-data">
-            <input type="hidden" name="csrf_token" value="<?= Helpers\CrossSiteForgeryProtection::getToken() ?>">
-            <input type="hidden" name="user_id" value="<?= $queryUser->getUserId(); ?>">
-            <div class="sm:flex-col sm:items-start">
-              <h3 class="text-lg font-medium leading-6 text-gray-900" id="modal-title">Edit Profile</h3>
-              <div class="mt-2">
-                <p class="text-sm text-gray-500">Please fill in the following fields to update your profile.</p>
-              </div>
-              <div class="mt-2 relative">
-                <img src="<?= $queryUser->getImagePath(); ?>" alt="user-icon" class="size-20 brightness-90 rounded-full" id="previewImage">
-                <label for="upload-file" class="absolute top-5 left-5 hover:cursor-pointer bg-gray-700/70 hover:brightness-110 transition duration-300 p-2 rounded-full">
-                  <img src="/images/upload-icon.svg" alt="upload-icon">
-                  <input 
-                    id="upload-file" 
-                    type="file" 
-                    name="upload-file" 
-                    class="hidden"
-                    accept="image/png, image/jpg, image/jpeg, image/gif, image/webp"
-                  >
-                </label>
-              </div>
-              <div class="mt-2">
-                <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
-                <input type="text" id="username" name="username" class="p-2 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm" placeholder="Enter your username" value="<?php echo $queryUser->getUsername() ?>">
-              </div> 
-              <div class="mt-2">
-                <label for="age" class="block text-sm font-medium text-gray-700">Age</label>
-                <input type="number" id="age" name="age" class="p-2 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm" placeholder="Enter your age" value="<?php echo $queryUser->getAge() ?>">
-              </div>
-              <div class="mt-2">
-                <label for="address" class="block text-sm font-medium text-gray-700">Address</label>
-                <input type="text" id="address" name="address" class="p-2 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm" placeholder="Enter your address" value="<?php echo $queryUser->getAddress() ?>">
-              </div>
-              <div class="mt-2">
-                <label for="hobby" class="block text-sm font-medium text-gray-700">Hobby</label>
-                <input type="text" id="hobby" name="hobby" class="p-2 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm" placeholder="Enter your hobby" value="<?php echo $queryUser->getHobby() ?>">
-              </div>
-              <div class="mt-2">
-                <label for="self_introduction" class="block text-sm font-medium text-gray-700">Self Introduction</label>
-                <textarea id="self_introduction" name="self_introduction" rows="4" class="p-2 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm" placeholder="Enter your self introduction"><?php echo $queryUser->getSelfIntroduction() ?></textarea>
-              </div>
-            </div>
-          </div>
-          <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-            <button id="updateBtn" type="submit" class="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-blue-500 sm:ml-3 sm:w-auto">Submit</button>
-            <button id="cancelBtn" type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
-          </div>
-          </form>
+  <div class="fixed inset-0 flex items-center justify-center">
+
+    <!-- Edit Profile Card -->
+    <div class="w-[30rem] h-auto p-3 rounded-2xl bg-white">
+      <!-- Header -->
+      <div class="pb-3 pr-3 flex items-center">
+        <button
+          id="close-button"
+          type="button"
+          aria-label="Close" 
+          class="mr-10 size-8 flex items-center justify-center rounded-full cursor-pointer  transition duration-300 hover:bg-gray-400/50">
+          <img src="images/close-icon.svg" alt="" class="size-6">
+        </button>
+        <span class="text-lg font-semibold">プロフィールを編集</span>
       </div>
+
+      <!-- Profile Icon -->
+      <div class="py-5 flex items-center justify-center">
+
+        <!-- Preview Icon -->
+        <div class="relative size-36 rounded-full"> 
+          <img
+            id="preview-image"
+            src="<?= $queryUser->getImagePath(); ?>"
+            alt="user-icon" 
+            class="size-full object-fill rounded-full"
+          >
+          <div class="absolute inset-0 size-36 rounded-full bg-[rgba(0,0,0,0.3)]"></div>
+
+          <!-- Camera Icon -->
+          <form id="update-profile-icon-form" action="form/update/profile/icon" method="POST" enctype="multipart/form-data">
+            <input type="hidden" name="csrf_token" value="<?= Helpers\CrossSiteForgeryProtection::getToken() ?>">
+            <label for="upload-file">
+              <div class="absolute inset-13 flex items-center justify-center size-10 bg-black/70 rounded-full transition duration-300 brightness-95 cursor-pointer hover:brightness-110">
+                <img 
+                  src="/images/camera.svg" 
+                  alt="camera-icon" class="size-5"
+                >
+                <input 
+                  id="upload-file"
+                  type="file"
+                  name="upload-file"
+                  class="hidden"
+                  accept="image/png, image/jpg, image/jpeg, image/gif, image/webp"
+                >
+              </div>
+            </label>
+          </form>
+        </div>
+
+        <!-- Update profile icon button -->
+        <div id="update-profile-icon-button"></div>
+      </div>
+
+      <!-- Profile Information -->
+      <form action="form/update/profile" method="POST">
+        <input type="hidden" name="csrf_token" value="<?= Helpers\CrossSiteForgeryProtection::getToken() ?>">
+        <input type="hidden" name="user_id" value="<?= $queryUser->getUserId(); ?>">
+
+        <div class="pt-3 flex flex-col space-y-4 border-t border-slate-200">
+          <!-- data -->
+          <?php foreach($profileInfoList as $key => $data): ?>
+            <?php if($key === 'selfIntroduction') : ?>
+              <label for="<?= $data['name']; ?>" class="text-slate-500">
+                <?= $data['label']; ?>
+                <textarea 
+                  name="<?= $data['name']; ?>" 
+                  class="p-2 w-full rounded-md border border-gray-300 shadow-sm text-black"
+                  rows="<?= $data['rows']; ?>"
+                ><?= $data['value']; ?></textarea>
+              </label>
+            <?php else: ?>
+              <label for="<?= $data['name']; ?>" class="text-slate-500">
+                <?= $data['label']; ?>
+                <input 
+                  type="<?= $data['type']; ?>" 
+                  name="<?= $data['name']; ?>" 
+                  class="p-2 w-full rounded-md border border-gray-300 shadow-sm text-black"
+                  value="<?= $data['value']; ?>"
+                >
+              </label>
+            <?php endif; ?>
+          <?php endforeach; ?>
+          <div class="flex items-center justify-end">
+            <button
+              type="submit"
+              class="px-4 py-1.5 bg-black text-white text-sm font-semibold rounded-3xl cursor-pointer transition duration-300 hover:opacity-75"
+            >
+            保存
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </div>
@@ -61,9 +147,10 @@
 <script>
   document.addEventListener('DOMContentLoaded', function() {
     const modal = document.getElementById('modal');
-    const cancelBtn = document.getElementById('cancelBtn');
+    const closeBtn = document.getElementById('close-button');
+    const updateProfileIconBtn = document.getElementById('update-profile-icon-button');
 
-    cancelBtn.addEventListener('click', function() {
+    closeBtn.addEventListener('click', function() {
       modal.classList.add('hidden');
     });
 
@@ -73,10 +160,24 @@
       if (file) {
         const reader = new FileReader();
         reader.onload = function(e) {
-          const img = document.getElementById('previewImage');
+          const img = document.getElementById('preview-image');
           img.src = e.target.result;
         };
         reader.readAsDataURL(file);
+
+        if(updateProfileIconBtn) {
+          const updateBtn = document.createElement('button');
+          updateBtn.type = 'submit';
+          updateBtn.classList.add(
+            "ml-3", "py-2", "px-4", "text-sm", "bg-slate-500",
+            "text-white", "rounded-2xl", "cursor-pointer",
+            "transition", "duration-300", "hover:brightness-110"
+          );
+          updateBtn.innerText = 'アイコンを保存する';
+          updateBtn.setAttribute('form', 'update-profile-icon-form');
+  
+          updateProfileIconBtn.appendChild(updateBtn);
+        }
       }
     });
   });
