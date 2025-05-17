e508cc32901a2ffd53d22f964f9d9850b73ff7c8
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-17 20:52:30 +0900

    Add the UI when data is null

---
+++ Views/component/follower/user-list.php
@@ -0,0 +1,28 @@
+<div class="col-span-8 w-full h-full flex flex-col space-y-4 p-4 bg-white rounded-xl shadow-md">
+  <div class="flex items-center space-x-2">
+     <?php include "Views/component/undo.php" ?>
+    <div class="text-xl font-semibold"><?= $authUser->getUsername(); ?></div>
+  </div>
+
+  <?php if($data === null): ?>
+    <div class="flex flex-col space-y-2 items-center justify-center pt-10">
+      <p class="text-2xl font-semibold">
+        まだフォロワーがいません
+      </p>
+      <p class="text-sm text-slate-400">フォローされるとここに表示されます</p>
+    </div>
+  <?php else: ?>
+    <div class="flex flex-col divide-y divide-slate-200">
+      <?php foreach($data as $item): ?>
+      <div class="relative flex items-start space-x-4 p-4">
+        <a href="/profile?user=<?= $item->getUsername(); ?>" class="absolute inset-0 hover:bg-slate-400 opacity-20 transition duration-300"></a>
+        <img src="<?= $item->getImagePath(); ?>" alt="user-icon" class="w-12 h-12 rounded-full">
+        <div class="flex flex-col space-y-1">
+          <div class="font-medium text-lg"><?= $item->getUsername() ?></div>
+          <div class="text-sm text-gray-600"><?= $item->getSelfIntroduction(); ?></div>
+        </div>
+      </div>
+      <?php endforeach; ?>
+    </div>
+  <?php endif; ?>
+</div>
--- Views/page/following.php
+++ Views/page/following.php
@@ -1,5 +1,5 @@
 <div class="w-full h-full grid grid-cols-12">
   <?php include "Views/component/banner.php" ?>
-  <?php include "Views/component/followList.php" ?>
+  <?php include "Views/component/following/user-list.php" ?>
   <?php include "Views/component/sidebar.php" ?>
 </div>
