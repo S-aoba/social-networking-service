3ac0dc7fa34c39fb8e302c2896530aafc90ab6be
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-05-28 16:18:17 +0900

    Refactor required rule

---
+++ Validators/Rules/RequiredRule.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Validators\Rules;
+
+class RequiredRule
+{
+  public static function validate(string $field, $value): array
+  {
+    return isset($value) && $value !== '' ? 
+            [$field => $value] 
+            : 
+            throw new \InvalidArgumentException("{$field} is required.");
+  }
+}
