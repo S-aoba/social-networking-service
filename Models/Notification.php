8049e3796c6fc7811867a7ed18bc046fd7c55c00
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-06-03 18:55:27 +0900

    Run PHP-CS-Fixer

--- Models/Notification.php
+++ Models/Notification.php
@@ -8,77 +8,78 @@ use Models\Traits\GenericModel;
 
 class Notification implements Model
 {
-  use GenericModel;
+    use GenericModel;
 
-  const TYPES = ['follow', 'like', 'reply'];
+    public const TYPES = ['follow', 'like', 'reply'];
 
-  public function __construct(
-    private int $userId,
-    private string $type,
-    private ?array $data,
-    private ?string $reatAt = null,
-    private ?DataTimeStamp $timestamp = null
-  )
-  {
-    $this->checkType();
-  }
+    public function __construct(
+        private int $userId,
+        private string $type,
+        private ?array $data,
+        private ?string $reatAt = null,
+        private ?DataTimeStamp $timestamp = null
+    ) {
+        $this->checkType();
+    }
 
-  public function getUserId(): int
-  {
-    return $this->userId;
-  }
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
 
-  public function setUserId(int $userId): void
-  {
-    $this->userId = $userId;
-  }
+    public function setUserId(int $userId): void
+    {
+        $this->userId = $userId;
+    }
 
-  public function getType(): string
-  {
-    return $this->type;
-  }
+    public function getType(): string
+    {
+        return $this->type;
+    }
 
-  public function setType(string $type): void
-  {
-    $this->type = $type;
-  }
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
 
-  public function getData(): ?array
-  {
-    return $this->data;
-  }
+    public function getData(): ?array
+    {
+        return $this->data;
+    }
 
-  public function setData(?array $data): void
-  {
-    $this->data = $data;
-  }
+    public function setData(?array $data): void
+    {
+        $this->data = $data;
+    }
 
-  public function getReatAt(): ?string
-  {
-    return $this->reatAt;
-  }
+    public function getReatAt(): ?string
+    {
+        return $this->reatAt;
+    }
 
-  public function setReatAt(?string $reatAt): void
-  {
-    $this->reatAt = $reatAt;
-  }
+    public function setReatAt(?string $reatAt): void
+    {
+        $this->reatAt = $reatAt;
+    }
 
-  public function getTimestamp(): ?DataTimeStamp
-  {
-    return $this->timestamp;
-  }
+    public function getTimestamp(): ?DataTimeStamp
+    {
+        return $this->timestamp;
+    }
 
-  public function setTimestamp(?DataTimeStamp $timestamp): void
-  {
-    $this->timestamp = $timestamp;
-  }
+    public function setTimestamp(?DataTimeStamp $timestamp): void
+    {
+        $this->timestamp = $timestamp;
+    }
 
-  private function checkType(): void
-  {
-    $result = in_array($this->type, self::TYPES);
+    private function checkType(): void
+    {
+        $result = in_array($this->type, self::TYPES);
 
-    if($result === false) throw new Exception("Invalit notification type: {$this->type}");
+        if ($result === false) {
+            throw new Exception("Invalit notification type: {$this->type}");
+        }
 
-    return;
-  }
+        return;
+    }
 }
