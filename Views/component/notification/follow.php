24e09546d7eaf0f40abe663b3a071f591e3afbcc
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-06-03 14:28:34 +0900

    Add the prototype UI of notification page

---
+++ Views/component/notification/follow.php
@@ -0,0 +1,9 @@
+<div id="notification-item" class="py-2 flex min-h-20">
+  <div class="w-16 flex justify-end">
+    <img src="/images/follow-icon.svg" alt="follow-icon" class="size-7">
+  </div>
+  <div class="pl-2 flex flex-col space-y-2">
+    <img src="/images/default-icon.png" alt="user-icon" class="size-8">
+    <span class="text-sm">○○さんにフォローされました。</span>
+  </div>
+</div>
---
+++ Views/component/notification/like.php
@@ -0,0 +1,12 @@
+<div id="notification-item" class="py-2 flex min-h-20">
+  <div class="w-16 flex justify-end">
+    <img src="/images/like-icon.svg" alt="like-icon" class="size-7">
+  </div>
+  <div class="pl-2 flex flex-col space-y-2">
+    <img src="/images/default-icon.png" alt="user-icon" class="size-8">
+    <span class="text-sm">
+      ○○さんがあなたのポストをいいねしました。
+    </span>
+    <span class="text-sm">Postの中身</span>
+  </div>
+</div>
