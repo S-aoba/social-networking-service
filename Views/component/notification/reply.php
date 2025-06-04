3804008c107b4b3fb9265e759fc8191cc8c1069e
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-06-04 21:17:15 +0900

    Add message and redirect link in the notification item

--- Views/component/notification/reply.php
+++ Views/component/notification/reply.php
@@ -1,10 +1,16 @@
- <div id="notification-item" class="py-2 flex min-h-20">
-  <div class="w-16 flex justify-end">
-    <img src="/images/comment-icon.svg" alt="comment-icon" class="size-7">
+<?php
+$message = $notification->getData()['message'];
+$redirect = $notification->getData()['redirect'];
+?>
+
+<a href="<?= htmlspecialchars($redirect) ?>"> 
+  <div id="notification-item" class="py-2 flex min-h-20 hover:bg-slate-100 transition duration-300">
+    <div class="w-16 flex justify-end">
+      <img src="/images/comment-icon.svg" alt="comment-icon" class="size-7">
+    </div>
+    <div class="pl-2 flex flex-col space-y-2">
+      <img src="/images/default-icon.png" alt="user-icon" class="size-8">
+      <span class="text-sm"><?= htmlspecialchars($message); ?></span>
+    </div>
   </div>
-  <div class="pl-2 flex flex-col space-y-2">
-    <img src="/images/default-icon.png" alt="user-icon" class="size-8">
-    <span class="text-sm">○○さんから返信がありました。</span>
-    <span class="text-sm">Postの中身</span>
-  </div>
-</div>
+</a>
