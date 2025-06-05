6df9db37f2d913017cb39f8f895a7e714f62c165
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-06-05 12:36:35 +0900

    Fix indicator of notification UI

--- Views/component/navigation-item.php
+++ Views/component/navigation-item.php
@@ -5,7 +5,7 @@
             <?php if($hasNotification): ?>
                 <div 
                     id="indicator" 
-                    class="absolute top-2 left-6 w-3 h-3 bg-blue-500 rounded-full shadow-lg ring-2 ring-white animate-pulse z-10 border-2 border-blue-300"
+                    class="absolute top-2.5 left-6.5 size-2.5 bg-blue-500 rounded-full z-10"
                     style="box-shadow: 0 0 8px 2px rgba(59,130,246,0.5);"
                     title="新着通知あり"
                 ></div>
