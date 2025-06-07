eca0c4d02e64a5d66079c6c4959adb4afc61bc55
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-06-07 21:54:15 +0900

    Add notification count UI in the indicator component

--- Views/component/indicator.php
+++ Views/component/indicator.php
@@ -1,6 +1,8 @@
 <div 
     id="indicator" 
-    class="absolute top-2.5 left-6.5 size-2.5 bg-blue-500 rounded-full z-10"
+    class="absolute top-1.5 left-6 size-5 bg-blue-500 rounded-full z-10 flex items-center justify-center text-xs font-bold text-white border "
     style="box-shadow: 0 0 8px 2px rgba(59,130,246,0.5);"
     title="新着通知あり"
-></div>
+>
+2
+</div>
