b683af36e0ce9b756481521ff7297da082e8e9fe
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-06-07 21:43:54 +0900

    Add notification indicator component to navigation item

--- Views/component/navigation-item.php
+++ Views/component/navigation-item.php
@@ -3,12 +3,7 @@
 
         <?php if($item['label'] === 'notification'): ?>
             <?php if($hasNotification): ?>
-                <div 
-                    id="indicator" 
-                    class="absolute top-2.5 left-6.5 size-2.5 bg-blue-500 rounded-full z-10"
-                    style="box-shadow: 0 0 8px 2px rgba(59,130,246,0.5);"
-                    title="新着通知あり"
-                ></div>
+                <?php include "Views/component/indicator.php" ?>
             <?php endif; ?>
         <?php endif; ?>
             
