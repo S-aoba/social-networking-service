0f27ce6ef90ed915cb1c8fa7405679a1210dfc9c
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-06-10 12:12:16 +0900

    Change logic when res is error at the create dm API

--- public/js/dm-form.js
+++ public/js/dm-form.js
@@ -19,10 +19,26 @@ document.addEventListener("DOMContentLoaded", function () {
     if (data.status === "success") {
       window.location.href = data.redirect;
     } else {
-      const messages = data.message;
-
-      errorMessage.innerHTML = "<ul>" + Object.values(messages).map(msg => `<li>${msg}</li>`).join("") + "</ul>";
-      errorMessage.classList.remove("hidden");
+      showError(data.message);
     }
   });
+
+  function showError(message) {
+    if (typeof message === "string") {
+      errorMessage.textContent = message;
+    } else if (typeof message === "object") {
+      const ul = document.createElement("ul");
+      Object.keys(message).forEach((key) => {
+        const li = document.createElement("li");
+        li.classList.add("list-none");
+        li.innerText = message[key];
+        ul.appendChild(li);
+      });
+      errorMessage.appendChild(ul);
+    } else {
+      errorMessage.textContent =
+        "Something went wrong on our end. Please try again later.";
+    }
+    errorMessage.classList.remove("hidden");
+  }
 });
