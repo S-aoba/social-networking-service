8c430a8c7dae807a4403e8f8ea197c7ac9926330
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-06-10 12:08:43 +0900

    Change logic when res is error at the delete post API

--- public/js/delete-post-form.js
+++ public/js/delete-post-form.js
@@ -1,35 +1,51 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const deletePostForms = document.querySelectorAll('.delete-post-form');
+document.addEventListener("DOMContentLoaded", function () {
+  const deletePostForms = document.querySelectorAll(".delete-post-form");
 
-    deletePostForms.forEach((form) => {
-      form.addEventListener('submit', async(e) => {
-        e.preventDefault();
-  
-        const formData = new FormData(form);
+  deletePostForms.forEach((form) => {
+    form.addEventListener("submit", async (e) => {
+      e.preventDefault();
 
-        const errorMessage = document.querySelector('.delete-post-error-message');
-        errorMessage.textContent = '';
-        errorMessage.classList.add('hidden');
-  
-        const res = await fetch('api/delete/post', {
-          method: 'POST',
-          body: formData
-        });
-  
-        const data = await res.json();
-  
-        if(data.status === 'success') {
-          if(window.location.pathname.startsWith('/post')) {
-            window.location.href = '/';
-          } else {
-            window.location.reload();
-          }
+      const formData = new FormData(form);
+
+      const errorMessage = form.querySelector(".delete-post-error-message");
+      errorMessage.textContent = "";
+      errorMessage.classList.add("hidden");
+
+      const res = await fetch("api/delete/post", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await res.json();
+
+      if (data.status === "success") {
+        if (window.location.pathname.startsWith("/post")) {
+          window.location.href = "/";
         } else {
-          const messages = data.message;
- 
-          errorMessage.innerHTML = "<ul>" + Object.values(messages).map(msg => `<li>${msg}</li>`).join("") + "</ul>";
-          errorMessage.classList.remove("hidden");
+          window.location.reload();
         }
-      })
-    })
-  })
+      } else {
+        showError(data.message, errorMessage)
+      }
+    });
+  });
+
+  function showError(message, errorMessage) {
+    if (typeof message === "string") {
+      errorMessage.textContent = message;
+    } else if (typeof message === "object") {
+      const ul = document.createElement("ul");
+      Object.keys(message).forEach((key) => {
+        const li = document.createElement("li");
+        li.classList.add("list-none");
+        li.innerText = message[key];
+        ul.appendChild(li);
+      });
+      errorMessage.appendChild(ul);
+    } else {
+      errorMessage.textContent =
+        "Something went wrong on our end. Please try again later.";
+    }
+    errorMessage.classList.remove("hidden");
+  }
+});
