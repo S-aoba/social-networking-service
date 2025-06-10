7c5e80b386acc8575d3942a91df55c937904e724
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-06-10 11:38:17 +0900

    Change logic when res is error at the create conversation API

--- public/js/compose-conversation-form.js
+++ public/js/compose-conversation-form.js
@@ -1,32 +1,49 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const conversationForms = document.querySelectorAll('.create-conversation-form');
-  const errorMessage = document.getElementById('conversation-error-message');
+document.addEventListener("DOMContentLoaded", () => {
+  const conversationForms = document.querySelectorAll(
+    ".create-conversation-form"
+  );
+  const errorMessage = document.getElementById("conversation-error-message");
 
   conversationForms.forEach((form) => {
-    form.addEventListener('click', async(e) => {
+    form.addEventListener("click", async (e) => {
       e.preventDefault();
 
       const formData = new FormData(form);
-      errorMessage.textContent = '';
-      errorMessage.classList.add('hidden');
+      errorMessage.textContent = "";
+      errorMessage.classList.add("hidden");
 
-      const res = await fetch('api/conversation', {
+      const res = await fetch("api/conversation", {
         method: "POST",
-        body: formData
+        body: formData,
       });
 
       const data = await res.json();
 
-      if(data.status === 'success') {
+      if (data.status === "success") {
         // Modalを閉じる処理
         window.location.href = data.redirect;
+      } else {
+        showError(data.message);
       }
-      else {
-        const messages = data.message;
+    });
+  });
 
-        errorMessage.innerHTML = "<ul>" + Object.values(messages).map(msg => `<li>${msg}</li>`).join("") + "</ul>";
-        errorMessage.classList.remove("hidden");
-      }
-    })
-  })
-})
+  function showError(message) {
+    if (typeof message === "string") {
+      errorMessage.textContent = message;
+    } else if (typeof message === "object") {
+      const ul = document.createElement("ul");
+      Object.keys(message).forEach((key) => {
+        const li = document.createElement("li");
+        li.classList.add("list-none");
+        li.innerText = message[key];
+        ul.appendChild(li);
+      });
+      errorMessage.appendChild(ul);
+    } else {
+      errorMessage.textContent =
+        "Something went wrong on our end. Please try again later.";
+    }
+    errorMessage.classList.remove("hidden");
+  }
+});
